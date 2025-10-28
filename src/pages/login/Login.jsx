846cccb8bf0b5a08bf23307339a9c295be75a23d
Mahmoud <forkan.saadaoui@gmail.com>
2025-10-28 14:28:59 +0100

    update login

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -39,7 +39,7 @@ function Login() {
     <div className="login">
       <div className="loginWrapper">
         <div className="loginLeft">
-          <h3 className="loginLogo">Social App</h3>
+          <h3 className="loginLogo">Facebook Clone</h3>
           <span className="loginDesc">
             Connect with friends and the world around you on Social App.
           </span>
